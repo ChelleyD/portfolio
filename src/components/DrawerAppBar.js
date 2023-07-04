1f72729150c0b7c854637235e29f70a56292a2dc
Tamika Dantes <chelley.tamz@gmail.com>
2023-07-04 17:01:20 -0400

    fixed babel bug warning

--- src/components/DrawerAppBar.js
+++ src/components/DrawerAppBar.js
@@ -10,8 +10,6 @@ import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import { Typography } from '@mui/material';
-import { styled } from '@mui/material/styles';
 
 const drawerWidth = 240;
 const navItems = ['Home', 'Projects', 'Contact'];
