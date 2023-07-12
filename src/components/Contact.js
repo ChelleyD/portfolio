8a58921d488ad303cf56766cc043c8b056cc6665
Tamika Dantes <chelley.tamz@gmail.com>
2023-07-12 11:44:16 -0400

    added rel attribute to social links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -90,15 +90,15 @@ const Contact = () => {
         </Typography>
 
         <Stack direction="row" spacing={2} sx={{justifyContent: 'center', alignItems: 'center'}}>
-          <a href='https://www.linkedin.com/in/tamika-dantes' target='_blank' className='link'>
+          <a href='https://www.linkedin.com/in/tamika-dantes' target='_blank' rel="noreferrer" className='link'>
             <LinkedInIcon fontSize='large' id='before-form'/>
           </a>
 
-          <a href='https://github.com/ChelleyD' target='_blank' className='link'>
+          <a href='https://github.com/ChelleyD' target='_blank' rel="noreferrer" className='link'>
             <GitHubIcon fontSize='large' id='before-form'/>
           </a>
 
-          <a href='mailto: tamika.dantes@gmail.com' target='_blank' className='link'>
+          <a href='mailto: tamika.dantes@gmail.com' target='_blank' rel="noreferrer" className='link'>
             <EmailIcon fontSize='large' id='before-form'/>
           </a>
             
