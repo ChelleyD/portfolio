e25dd19f6dadab6ab42a435b8a401106b66c6964
Tamika Dantes <chelley.tamz@gmail.com>
2023-08-17 11:53:31 -0400

    updated case study demo videos

--- src/components/Projects/PP5.js
+++ src/components/Projects/PP5.js
@@ -182,14 +182,14 @@ const PP5 = () => {
             <Divider />
             <div className="casestudy-section-body">
               <Typography gutterBottom></Typography>
-              {/* <iframe
+              <iframe
                 width="100%"
                 style={{ aspectRatio: "16/9" }}
-                src="https://www.youtube.com/embed/AkknY9nAYak"
+                src="https://www.youtube.com/embed/0V1sufEAJ_A"
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoPlay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              ></iframe> */}
+              ></iframe>
               {/* <a
                 // live demo link
                 href="https://ttp-code-quest.netlify.app/"
